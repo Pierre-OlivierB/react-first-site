37a94f2a047de6962f3664deab4b15ac73808322
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-12-28 13:23:29 +0100

    add project content logic and random circle

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Buttons = (props) => {
-  console.log(props);
+  // console.log(props);
   return (
     <div className="scroll-bottom">
       {props.left && (
